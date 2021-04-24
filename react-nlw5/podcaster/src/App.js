f5c5c984cac69ef881523e8054003fb529d3a79f
juperola2 <shih.marg@gmail.com>
2021-04-24 14:03:19 -0400

    propriedade dos componentes

--- react-nlw5/podcaster/src/App.js
+++ react-nlw5/podcaster/src/App.js
@@ -1,6 +1,15 @@
+import Button from "./Button";
+
+// <> </> tag html vazia - Conceito Fragment
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>  
+      <h1>Hello World!</h1>
+      <Button title="Botão 1"></Button>
+      <Button title="Botão 2"></Button>
+      <Button>Botão 3</Button>
+    </>
   );
 }
 
