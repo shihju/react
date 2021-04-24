f64e75b74b85c30ad2b0b650a89c579b84620d50
juperola2 <shih.marg@gmail.com>
2021-04-24 17:43:59 -0400

    adding header to all pages

--- react-nlw5/podcaster-next/src/pages/_app.tsx
+++ react-nlw5/podcaster-next/src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import '../styles/global.scss';
+import { Header } from "../components/Header";
+
+// Coisas que estão sempre em toda aplicação devem estar no app
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp
