e6923b5cbc7f44a949852cdd7e0a94f0406ce29b
juperola2 <shih.marg@gmail.com>
2021-04-24 14:23:02 -0400

    setting counter on button

--- react-nlw5/podcaster/src/Button.js
+++ react-nlw5/podcaster/src/Button.js
@@ -1,5 +1,20 @@
+import {useState} from 'react';
+
 export default function Button(props) {
+
+    const [counter, setCounter] = useState(1);
+    // [estado, funcaoQueVaiAlterarEstado]
+
+    function increment() {
+        setCounter(counter + 1);
+        console.log(counter);
+    }
+
     return (
-        <button>{props.children}</button>
+        <>
+            <span>{counter}</span>
+            <button onClick={increment}>{props.children}</button>
+            <br/>
+        </>
     );
 }
