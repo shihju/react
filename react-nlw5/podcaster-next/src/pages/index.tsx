dae70555f7cd12c1c4ff1f121452d805de843ffa
juperola2 <shih.marg@gmail.com>
2021-04-24 22:41:50 -0400

    usage of axios

--- react-nlw5/podcaster-next/src/pages/index.tsx
+++ react-nlw5/podcaster-next/src/pages/index.tsx
@@ -5,6 +5,7 @@
 
 import { useEffect } from "react";
 import { GetStaticProps } from 'next';
+import { api } from "../services/api";
 
 type Episode = {
   id: string;
@@ -54,8 +55,14 @@ export default function Home(props: HomeProps) {
 // yarn build
 // yarn start
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch('http://localhost:3333/episodes?_limit=12&_sort=published_at&_order=desc');
-  const data = await response.json();
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+
 
   return {
     props: {
